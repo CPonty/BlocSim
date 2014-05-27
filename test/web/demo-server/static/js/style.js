66a851751c840ac2b708783f59766b9899c09505
CPonty <christopher.ponticello@uqconnect.edu.au>
2014-05-27 17:31:38 +1000

    Fixed some buggy UI formatting on window resize. Left-hand side is now a pop-out panel with info arranged in collapsible accordions. Worked out how to embed 'fake' buttons (just inert text) in toolbar button rows. Laid out structure for Simulation page (text-based; very straightforward)

--- test/web/demo-server/static/js/style.js
+++ test/web/demo-server/static/js/style.js
@@ -16,9 +16,10 @@ $(function() {
   /* Apply JQuery UI styling & actions */
   $( "input[type=submit], button, .button" ).button();
 
+  // generate icons for buttons with shorthand classnames
   var button_icons = [
     "pause", "close", "refresh", "power", "transferthick-e-w",
-    "triangle-1-s"
+    "triangle-1-s", "stop", "play", "triangle-1-w", "triangle-1-e"
   ];
   for (var i=0; i<button_icons.length; i++) {
     $( "."+button_icons[i]+"-text" ).button({
@@ -28,6 +29,26 @@ $(function() {
       text: false, icons: {primary: "ui-icon-"+button_icons[i]}
     });
   }
+  // remove all effects, events from 'fake' buttons  
+  $('.not-button').unbind();
+  /*
+  $( ".not-button" ).click(function() { 
+    $(this).removeClass("ui-state-active"); 
+    return false; 
+  });
+  */
+  //$( ".not-button" ).off("click");
+  //$( ".not-button" ).removeClass("ui-state-active"); 
+  /*
+  $( ".not-button" ).focus(function () {
+    $(this).removeClass("ui-state-focus");
+  });
+  */
+  /*
+  $( ".not-button" ).active(function () {
+    $(this).removeClass("ui-state-active");
+  });
+  */
   /*
   $( ".pause-icon" ).button({text: false, icons: {primary: "ui-icon-pause"}});
   $( ".pause-text" ).button({text: true, icons: {primary: "ui-icon-pause"}});
@@ -53,6 +74,53 @@ $(function() {
   });
   */
 
+  /* Bind some click events */
+  // for each accordion hyperlink, hide/show the matching div
+  // can use -sidebar-group for the whole block, or just
+  // -sidebar-table to preserve the control bar
+  $( ".custom-accordion" ).click(function() {
+    var name = this.id.toString().split("-")[0];
+    $( "#"+name+"-sidebar-table" ).toggle();
+  });
+  // do/don't start collapsed
+  if (false)
+    $( ".sidebar-table" ).hide();
+
+  // setup the collapse/expand button for the sidebar
+  $( "#sidebar-collapsebtn" ).button().click(function() {
+    var myLeft = $(this).position().left;
+    var rightLeft = $(".right-wrap").position().left;
+    var pixelDistance = $(".left").width()
+    if (myLeft > 100) {
+      // already expanded - collapse
+      $(".left").toggle();
+      $(this).css({left: "0"});
+      $(".right-wrap").css({left: rightLeft-pixelDistance*1.1});
+      $(this).button({ icons: { primary: "ui-icon-triangle-1-e" } });
+    } else {
+      // already collapsed - expand
+      $(".left").toggle();
+      $(this).css({left: "25.5em"});
+      $(".right-wrap").css({left: rightLeft+pixelDistance*1.1});
+      $(this).button({ icons: { primary: "ui-icon-triangle-1-w" } });
+    }
+  });
+  // the button itself only appears when hovering on the sidebar
+  
+  $('#sidebar-collapsebtn').css('visibility', 'hidden');
+  $('.left').mouseenter(function() {
+      $('#sidebar-collapsebtn').css('visibility', 'visible');
+  }).mouseleave(function() {
+    if ($('#sidebar-collapsebtn').position().left > 100)
+      $('#sidebar-collapsebtn').css('visibility', 'hidden');
+  });
+  $('#sidebar-collapsebtn').mouseenter(function() {
+      $('#sidebar-collapsebtn').css('visibility', 'visible');
+  }).mouseleave(function() {
+    if ($(this).position().left > 100)
+      $('#sidebar-collapsebtn').css('visibility', 'hidden');
+  });
+  
   /*
   $('#whichTabBtn').click(function(e){
     alert($("#tabs").tabs('option', 'active'));
@@ -65,7 +133,7 @@ $(function() {
   
   /* UI events */
 
-  // operate the CV resolution select
+  // setup the dropdown menus
   $( "#cv-sidebar-resolution-dropdown" ).button().click(function() { //dropdownbtn
     var menu = $(this).parent().next().show().position({ //theOptionsListDiv
       my: "left top",
